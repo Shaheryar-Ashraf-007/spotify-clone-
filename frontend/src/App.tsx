6861539ce23435b4e5ac5b9b33850d2d922b46cf
Shaheryar-Ashraf-007 <shaheryarashraf824@gmail.com>
2025-04-26 01:32:45 +0500

    setup shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
+import { Button } from "./components/ui/button"
 
 function App() {
   return (
     <>
      <h1 className="bg-black text-white text-center">hello</h1>
+     <Button variant={"outline"}>THIS IS A BUTTON </Button>
     </>
   )
 }
